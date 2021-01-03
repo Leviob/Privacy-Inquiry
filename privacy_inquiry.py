bdfdfc359cdd3ea41e52e30cc10a717cfb565c60
Levi O <commonnightingale@pm.me>
2021-01-02 23:57:52 -0800

    Initial commit

---
+++ privacy_inquiry.py
@@ -0,0 +1,119 @@
+#! /usr/bin/python3
+
+from dotenv import load_dotenv
+load_dotenv()
+import json, requests, sys, pprint, datetime, os
+from tabulate import tabulate
+
+apikey = os.environ.get('API_KEY')
+
+def main():
+    if len(sys.argv) < 2:
+        print('include transaction amount in cents, or date ([yyyy/]mm/dd) as an argument')
+        sys.exit()
+         
+        # For troublshooting:
+        # print_transactions() 
+
+    # Distinguish whether date or amount were provided in CL argument 
+    if sys.argv[1].isnumeric() or (sys.argv[1][0] == '-' and sys.argv[1][1:].isnumeric()): #if purely numeric argument, search for tranasctions with this amount
+        search_by_amount(int(sys.argv[1]))
+
+    elif '/' in sys.argv[1]:
+        if sys.argv[1].count('/') == 1:
+            date = str(datetime.date.today().year) + '/' + sys.argv[1]
+        elif sys.argv[1].count('/') == 2:
+            date = sys.argv[1]
+        else:
+            print('Invalid format. Give amount in cents, or date in mm/dd or yyyy/mm/dd format')
+            sys.exit()
+        datetime_object = datetime.datetime.strptime(date, '%Y/%m/%d')
+        search_by_date(str(datetime_object.strftime('%Y-%m-%d')))
+
+    # List some or all recent transactions
+    elif 'ls' in sys.argv[1]:
+        if len(sys.argv) > 2 and sys.argv[2].isnumeric():
+            list_transactions(int(sys.argv[2]))
+        else:
+            list_transactions(1000)
+        sys.exit()
+    else: 
+        print('Invalid format. Give amount in cents, or date in mm/dd or yy/mm/dd format')
+        sys.exit()
+
+# For troubleshooting - displays formatted json data
+def print_transactions(): 
+    transactions_url='https://api.privacy.com/v1/transaction?begin=2020-01-01&page_size=1000' # Only the first 1000 transactions
+    response = requests.get(transactions_url, headers={'Authorization':f'api-key {apikey}'})
+    response.raise_for_status()
+    # print(response.text)
+    # load json to python variable
+    pprint.pprint(json.loads(response.text))
+    sys.exit()
+# Downloads transaction history using Privacy.com's API
+def download_transactions():
+    transactions_url='https://api.privacy.com/v1/transaction?&page_size=1000'  # Only the first 1000 transactions
+    response = requests.get(transactions_url, headers={'Authorization':f'api-key {apikey}'})
+    response.raise_for_status()
+    return json.loads(response.text)
+
+# Prints tabulated list of the passed number of most recent transactions 
+def list_transactions(num_to_show):
+    transaction_data = download_transactions()
+    results = [['Vendor', 'Amount', 'Date']]
+    if len(transaction_data['data']) < num_to_show:
+        num_to_show = len(transaction_data['data'])
+    for transaction in transaction_data['data'][:num_to_show]:
+        results.append([f'{transaction["merchant"]["descriptor"]}', f'${transaction["amount"]/100}', f'{transaction["created"][:10]}'])
+    print (tabulate(results))
+
+# Searches downloaded data for transactions made on passed date
+def search_by_date(transaction_date):
+    transaction_data = download_transactions()    
+    for transaction in transaction_data['data']: #Iterates over 'data', whos value is a list of dictionaries, each of which is a transaction
+        if str(transaction['created'])[:10] == transaction_date:
+            merch_descriptor = transaction['merchant']['descriptor']
+            dollar_amount = transaction['amount']/100
+            datetime_object = datetime.datetime.strptime(transaction["created"], '%Y-%m-%dT%H:%M:%SZ')
+            date_long = datetime_object.strftime('%B %d, %Y')
+    
+            # Return matching dates and amounts
+            print(f'{merch_descriptor} was paid ${dollar_amount} on {date_long}')
+            
+            # Return more information if requested
+            if len(sys.argv) > 2 and (sys.argv[2].lower() == 'more' or sys.argv[2].lower() == 'm'):
+                trans_status = transaction['status']
+                trans_result = transaction['result']
+                card_name = transaction['card']['memo']
+                merch_city = transaction['merchant']['city']
+                merch_state = transaction['merchant']['state']
+                print(f'This transaction is {trans_result} and {trans_status}')
+                print(f'The card used is named: {card_name}')
+                print(f'{merch_descriptor} is located in {merch_city}, {merch_state}\n')
+
+# Searches downloaded data for transactions of passed amount
+def search_by_amount(amount):
+    transaction_data = download_transactions()    
+    # Search by amount:
+    for transaction in transaction_data['data']:
+        if transaction['amount'] == amount:
+            merch_descriptor = transaction['merchant']['descriptor']
+            dollar_amount = transaction['amount']/100
+            datetime_object = datetime.datetime.strptime(transaction["created"], '%Y-%m-%dT%H:%M:%SZ')
+            date_long = datetime_object.strftime('%B %d, %Y')
+    
+            # Return matching dates and amounts
+            print(f'{merch_descriptor} was paid ${dollar_amount} on {date_long}')
+                
+            # Return more information if requested
+            if len(sys.argv) > 2 and (sys.argv[2].lower() == 'more' or sys.argv[2].lower() == 'm'):
+                trans_status = transaction['status']
+                trans_result = transaction['result']
+                card_name = transaction['card']['memo']
+                merch_city = transaction['merchant']['city']
+                merch_state = transaction['merchant']['state']
+                print(f'This transaction is {trans_result} and {trans_status}')
+                print(f'The card used is named: {card_name}')
+                print(f'{merch_descriptor} is located in {merch_city}, {merch_state}\n')
+
+main()
